e178d0361957fea2646e6fb7188e9a3cfed6201b
rfigg24 <rubenfigge24@gmail.com>
2023-03-20 21:35:06 +0100

    split training and validation

--- MNIST_Fashion_CNN_Models.py
+++ MNIST_Fashion_CNN_Models.py
@@ -1,9 +1,12 @@
 from keras.datasets import fashion_mnist
+from sklearn.model_selection import train_test_split
+import pandas as pd
 #loading the data:
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-#splitting the training data into a training and validation set with a 8 to 2 ratio:
-#We want the validation set to generalize over the whole data, so we want the same distribution of classes in our validation set, 
-#since the class is uniformly distributed we will take 1200 of each class and put them into our validationset:
+#splitting the training data into a training and validation set with a 8 to 2 ratio, while keeping the same class distribution be using stratify:
+x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train,random_state = 0, stratify=y_train, test_size=0.20)
+
+
 
 
