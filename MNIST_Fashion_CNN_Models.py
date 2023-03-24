c7ce232ab68c01c8b77c5e22441746d355f9134d
rfigg24 <rubenfigge24@gmail.com>
2023-03-24 11:23:44 +0100

    small change

--- MNIST_Fashion_CNN_Models.py
+++ MNIST_Fashion_CNN_Models.py
@@ -13,21 +13,31 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 #splitting the training data into a training and validation set with a 8 to 2 ratio, while keeping the same class distribution be using stratify:
 x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train,random_state = 0, stratify=y_train, test_size=0.20)
 
-#normalizing the images: for computational convienence
+
+
+#normalizing the images: to avoid to large output values that can produce a vanishing gradientl
 x_train = x_train / 255.0
 x_validate = x_validate / 255.0
 x_test = x_test / 255.0
 
+
+
 #BaseLine model:
 baseModel = tf.keras.Sequential([
+
+    tf.keras.layers.
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
+    tf.keras.layers.Dense(10),
+    tf.keras.layers.Softmax()
 ])
+
+
 
 baseModel.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
+
 callback = baseModel.fit(x_train, y_train, epochs=3)
 print(callback.history.keys())
 
