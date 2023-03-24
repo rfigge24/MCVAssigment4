1a9ca2a99f9efcb8948e607b8e9b92ceb75e7dd1
rfigge24 <rubenfigge24@gmail.com>
2023-03-24 17:36:49 +0100

    now plots get saved to file

--- visualizationPlotting.py
+++ visualizationPlotting.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 
-def plotPerformance(callback):
+def plotPerformance(callback, modelname):
     loss = callback.history['loss']
     val_loss = callback.history['val_loss']
     acc = callback.history['accuracy']
@@ -9,13 +9,15 @@ def plotPerformance(callback):
 
     plt.plot(epochs, loss, label='Training loss')
     plt.plot(epochs, val_loss, label='Validation loss')
-    plt.title('Training and validation loss')
+    plt.title(f'Training and validation loss for {modelname}')
     plt.legend()
-    
+    plt.savefig(f'{modelname}/LossPlot.png')
     plt.figure()
 
     plt.plot(epochs, acc, label='Training accuracy')
     plt.plot(epochs, val_acc, label='Validation accuracy')
-    plt.title('Training and validation accuracy')
+    plt.title(f'Training and validation accuracy for {modelname}')
     plt.legend()
+    plt.savefig(f'{modelname}/AccuracyPlot.png')
     plt.show()
+  
