fd2427c38d58df04f37890f66b47bbda7f7f1882
rfigge24 <rubenfigge24@gmail.com>
2023-03-28 12:13:55 +0200

    seperate files

---
+++ baseModel.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+from keras.layers import LeakyReLU
+
+# -------- base model --------
+baseModel = tf.keras.Sequential()         #val_loss: 0.2572 - val_accuracy: 0.9137
+
+#Adding layers to the base model:
+baseModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 6,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    input_shape = (28, 28, 1)
+    )
+)
+baseModel.add(
+    tf.keras.layers.MaxPooling2D()
+)
+baseModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 16,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    )
+)
+baseModel.add(
+    tf.keras.layers.MaxPooling2D()
+)
+baseModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 32,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    )
+)
+baseModel.add(
+    tf.keras.layers.Flatten()
+)
+baseModel.add(
+    tf.keras.layers.Dense(
+    64,
+    activation = 'relu'
+    )
+)
+baseModel.add(
+    tf.keras.layers.Dense(
+    120,
+    activation = 'relu'
+    )
+)
+baseModel.add(
+    tf.keras.layers.Dense(
+    80,
+    activation = 'relu'
+    )
+)
+baseModel.add(
+    tf.keras.layers.Dense(
+    10,
+    activation=tf.keras.activations.softmax
+    )
+)
+#Compiling Base Model:
+baseModel.compile(optimizer= tf.keras.optimizers.Adam(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
---
+++ variantModels.py
@@ -0,0 +1,145 @@
+import tensorflow as tf
+from keras.layers import LeakyReLU
+
+# -------- Dropout Model --------
+dropoutModel = tf.keras.Sequential()
+
+#Adding layers to the dropout model:
+dropoutModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 6,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    input_shape = (28, 28, 1)
+    )
+)
+dropoutModel.add(
+    tf.keras.layers.MaxPooling2D()
+)
+dropoutModel.add(
+    tf.keras.layers.Dropout(0.3)
+)
+dropoutModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 16,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    )
+)
+dropoutModel.add(
+    tf.keras.layers.MaxPooling2D()
+)
+dropoutModel.add(
+    tf.keras.layers.Dropout(0.1)
+)
+dropoutModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 32,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    )
+)
+dropoutModel.add(
+    tf.keras.layers.Flatten()
+)
+dropoutModel.add(
+    tf.keras.layers.Dense(
+    64,
+    activation = 'relu'
+    )
+)
+dropoutModel.add(
+    tf.keras.layers.Dense(
+    120,
+    activation = 'relu'
+    )
+)
+dropoutModel.add(
+    tf.keras.layers.Dense(
+    80,
+    activation = 'relu'
+    )
+)
+dropoutModel.add(
+    tf.keras.layers.Dense(
+    10,
+    activation=tf.keras.activations.softmax
+    )
+)
+#Compiling Base Model:
+dropoutModel.compile(optimizer= tf.keras.optimizers.Adam(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+# -------- generalisation model --------
+batchNormModel = tf.keras.Sequential()
+
+#Adding layers to the base model:
+batchNormModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 6,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    input_shape = (28, 28, 1)
+    )
+)
+batchNormModel.add(
+    tf.keras.layers.BatchNormalization()
+)
+batchNormModel.add(
+    tf.keras.layers.MaxPooling2D()
+)
+batchNormModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 16,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    )
+)
+batchNormModel.add(
+    tf.keras.layers.MaxPooling2D()
+)
+batchNormModel.add(
+    tf.keras.layers.Conv2D(
+    filters = 32,
+    kernel_size = 5,
+    padding = 'same',
+    activation = 'relu',
+    )
+)
+batchNormModel.add(
+    tf.keras.layers.Flatten()
+)
+batchNormModel.add(
+    tf.keras.layers.Dense(
+    64,
+    activation = 'relu'
+    )
+)
+batchNormModel.add(
+    tf.keras.layers.Dense(
+    120,
+    activation = 'relu'
+    )
+)
+batchNormModel.add(
+    tf.keras.layers.Dense(
+    80,
+    activation = 'relu'
+    )
+)
+batchNormModel.add(
+    tf.keras.layers.Dense(
+    10,
+    activation=tf.keras.activations.softmax
+    )
+)
+#Compiling Base Model:
+batchNormModel.compile(optimizer= tf.keras.optimizers.Adam(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
